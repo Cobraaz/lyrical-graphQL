1a26b94c2d16fdefcec14353be71d3a99596703d
Cobraaz <anujbansal077@gmail.com>
2020-10-26 15:23:10 +0530

    Added mutations

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
-import { Router, Route, hashHistory, IndexRoute } from "react-router";
 
 import App from "./components/App";
 import SongList from "./components/SongList";
@@ -16,7 +16,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
-          <Route path="song/new" component={SongCreate} />
+          <Route path="songs/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
